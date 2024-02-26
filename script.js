0b4da93b91a7ef2c054f6aba5699eb57cff71b45
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-26 16:09:37 +0100

    [add]  MILESTONE2

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const imageBig = document.querySelector('.image-big');
 for (let i = 0; i < immagini.length; i++) {
   console.log (i);
   // richiamo le immagini dell'array con la const img
-  const img = immagini [i];
+  const img = immagini[i];
+  console.log (immagini);
   // inserisco le immagini nell'html 
-  imageBig.innerHTML += '<img class="hide" src="${img}">';
+  imageBig.innerHTML += `<img class="img hide" src="${img}">`;
  
 }
-
+// richiamo la classe img per inserirla nella imgCollection
 const imgCollection = document.getElementsByClassName('img');
-
+// contatore immagini
 let countersImg = 0;
 // rimuovo HIDE dal primo elemento
-imgCollection[countersImg].classList.remove('hide');
+imgCollection [0].classList.remove('hide');
